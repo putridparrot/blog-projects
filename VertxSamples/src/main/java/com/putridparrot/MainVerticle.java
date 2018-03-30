2784026128e0c6957195ab6a6c43bb53fab7effa
Mark Timmings <mark.timmings@putridparrot.com>
2018-03-30 17:22:23 +0100

    Added logging

--- VertxSamples/src/main/java/com/putridparrot/MainVerticle.java
+++ VertxSamples/src/main/java/com/putridparrot/MainVerticle.java
@@ -3,8 +3,13 @@ package com.putridparrot;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+
 
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
@@ -19,6 +24,9 @@ public class MainVerticle extends AbstractVerticle {
         Router router = Router.router(vertx);
 
         router.route("/hello").handler(ctx -> {
+
+            LOGGER.debug("Hello called");
+
             ctx.response()
                     .putHeader("content-type", "text/plain")
                     .end("Hello " + ctx.queryParam("name"));
