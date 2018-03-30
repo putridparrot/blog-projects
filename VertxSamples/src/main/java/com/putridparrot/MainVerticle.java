0603331ff909cd7672aad8f095567d3370664c6f
Mark Timmings <mark.timmings@putridparrot.com>
2018-03-30 18:49:06 +0100

    Added Dockerfile and extracted Hello server and created World server.

--- VertxSamples/src/main/java/com/putridparrot/MainVerticle.java
+++ VertxSamples/src/main/java/com/putridparrot/MainVerticle.java
@@ -3,39 +3,24 @@ package com.putridparrot;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
-
 
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
 
-        vertx.deployVerticle(new MainVerticle());
+        initialize(vertx);
     }
-
 
     @Override
     public void start() {
+        initialize(vertx);
+    }
 
+    private static void initialize(Vertx vertx) {
         Router router = Router.router(vertx);
 
-        router.route("/hello").handler(ctx -> {
-
-            LOGGER.debug("Hello called");
-
-            ctx.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello " + ctx.queryParam("name"));
-        });
-
-        vertx.createHttpServer()
-                .requestHandler(router::accept)
-                .listen(8080);
-
-        System.out.println("HTTP server started on port 8080");
+        vertx.deployVerticle(new HelloVerticle(router));
+        vertx.deployVerticle(new WorldVerticle(router));
     }
 }
---
+++ VertxSamples/src/main/java/com/putridparrot/WorldVerticle.java
@@ -0,0 +1,36 @@
+package com.putridparrot;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorldVerticle extends AbstractVerticle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WorldVerticle.class);
+
+    private final Router router;
+
+    public WorldVerticle(Router router) {
+        this.router = router;
+    }
+
+    @Override
+    public void start() {
+
+        router.route("/world").handler(ctx -> {
+
+            LOGGER.debug("World called");
+
+            ctx.response()
+                    .putHeader("content-type", "text/plain")
+                    .end("World " + ctx.queryParam("name"));
+        });
+
+        vertx.createHttpServer()
+                .requestHandler(router::accept)
+                .listen(8080);
+
+        LOGGER.info("HTTP \"World\" server started on port 8080");
+    }
+}
