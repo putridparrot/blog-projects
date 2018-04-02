5806b440459ca6d2137bc877d048ffc28dc0d469
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2018-04-02 10:11:32 +0100

    Added code from blog post http://putridparrot.com/blog/using-giraffe-as-a-service-pipeline-in-kestrel/

---
+++ GiraffeTest/GiraffeTest/Program.fs
@@ -0,0 +1,31 @@
+﻿open Giraffe
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore
+ 
+// code for blog post http://putridparrot.com/blog/using-giraffe-as-a-service-pipeline-in-kestrel/
+
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                routeCif "/hello/%s" (fun name -> text (sprintf "Hello %s" name))
+            ]
+    ]
+ 
+type Startup() =
+    member this.ConfigureServices (services : IServiceCollection) =
+        services.AddGiraffe() |> ignore
+ 
+    member this.Configure (app : IApplicationBuilder) =
+        app.UseGiraffe webApp
+ 
+[<EntryPoint>]
+let main _ =
+    WebHost.CreateDefaultBuilder()
+        .UseKestrel()
+        .UseStartup<Startup>()
+        .Build()
+        .Run()
+    0
---
+++ GiraffeTest/READEME.md
@@ -0,0 +1,3 @@
+# F# .NET Core & Giraffe
+
+Covers code in blog post http://putridparrot.com/blog/using-giraffe-as-a-service-pipeline-in-kestrel/
