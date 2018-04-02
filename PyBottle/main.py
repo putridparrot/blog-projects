3d0ebd9c0fa6373692ce70183d9b2d4ab4273544
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2018-04-02 14:58:12 +0100

    Code for post Python REST service using Bottle (http://putridparrot.com/blog/python-rest-service-using-bottle/), added more routes and results returned in JSON

--- PyBottle/main.py
+++ PyBottle/main.py
@@ -1,9 +1,27 @@
-from bottle import route, run
+from bottle import route, run, get, post, error, request
 
-# pip install bottle
+import jsonpickle
+
+class Person:
+    def __init__(self):
+        self.firstName = "Eddie"
+        self.lastName = "Van Halen"
+
+# http://localhost:8080/putridparrot
 
 @route('/<name>')
 def index(name):
     return f"Hello {name}"
+
+# http://localhost:8080/person
+
+@get('/person')
+def person():
+    person = Person()
+    return jsonpickle.encode(person, unpicklable=False)
+
+@error(404)
+def error404(error):
+    return "Got a 404!"
 
 run(host='localhost', port=8080)
