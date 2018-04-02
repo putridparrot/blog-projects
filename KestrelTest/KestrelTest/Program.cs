e9142fbea55476b20d4bfc102683498b4a2016a6
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2018-04-02 15:59:37 +0100

    Extended with code from post More ASP.NET Core with C#  (http://putridparrot.com/blog/more-asp-net-core-with-c/)

--- KestrelTest/KestrelTest/Program.cs
+++ KestrelTest/KestrelTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -10,14 +12,45 @@ namespace KestrelTest
         public void Configure(IApplicationBuilder applicationBuilder,
             IHostingEnvironment hostingEnvironment)
         {
+            //applicationBuilder.Run(async context =>
+            //{
+            //    // http://localhost:5000/
+            //    await context.Response.WriteAsync("***Hello World***");
+            //});
+
+            // http://localhost:5000/hello/putridparrot
+            //applicationBuilder.Map("/hello", app =>
+            //{
+            //    app.Run(async ctx =>
+            //    {
+            //        var request =
+            //            ctx.Request.Path.HasValue ?
+            //                ctx.Request.Path.Value.Substring(1) :
+            //                String.Empty;
+            //        await ctx.Response.WriteAsync("Hello " + request);
+            //    });
+            //});
+
+            //  http://localhost:5000/hello?name=World
+            applicationBuilder.Map("/hello", app =>
+            {
+                app.Run(async ctx =>
+                {
+                    await ctx.Response.WriteAsync(
+                        "Hello " + ctx.Request.Query["name"]);
+                });
+            });
+
+            applicationBuilder.Map("/error", app =>
+            {
+                app.Run(ctx =>
+                    Task.FromResult(
+                        ctx.Response.StatusCode = 404)
+                );
+            });
+
             // http://localhost:5000/index.html
             applicationBuilder.UseStaticFiles();
-
-            applicationBuilder.Run(async context =>
-            {
-                // http://localhost:5000/
-                await context.Response.WriteAsync("***Hello World***");
-            });
         }
     }
 
