3d66b55d0d74e9fa189ae2170c3d2293445f26cf
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2018-04-02 15:48:45 +0100

    Code for Kestrel – ASP.NET core web server (http://putridparrot.com/blog/kestrel-asp-net-core-web-server/)

---
+++ KestrelTest/KestrelTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace KestrelTest
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder applicationBuilder,
+            IHostingEnvironment hostingEnvironment)
+        {
+            // http://localhost:5000/index.html
+            applicationBuilder.UseStaticFiles();
+
+            applicationBuilder.Run(async context =>
+            {
+                // http://localhost:5000/
+                await context.Response.WriteAsync("***Hello World***");
+            });
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>()
+                .UseKestrel()
+                .Build()
+                .Run();
+        }
+    }
+}
