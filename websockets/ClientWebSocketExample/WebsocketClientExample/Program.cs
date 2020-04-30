4483bc51826932df301f8aef186403b774e75f03
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2020-04-30 19:12:44 +0100

    Code for blog post http://putridparrot.com/blog/websockets-with-fleck/

---
+++ websockets/ClientWebSocketExample/WebsocketClientExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebsocketClientExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var websocketClient = new ClientWebSocket();
+            var cancellationToken = new CancellationTokenSource();
+
+            var connection = websocketClient.ConnectAsync(new Uri("ws://127.0.0.1:8181"), cancellationToken.Token);
+            connection.ContinueWith(async tsk =>
+            {
+                // sends a string/text message causes OnMessage to be called
+                await websocketClient.SendAsync(
+                    new ArraySegment<byte>(Encoding.UTF8.GetBytes("Hello World")),
+                    WebSocketMessageType.Text,
+                    true,
+                    cancellationToken.Token);
+
+                var buffer = new byte[128];
+                await websocketClient.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationToken.Token);
+                Console.WriteLine(Encoding.UTF8.GetString(buffer));
+
+                // sends a string/text message causes OnBinary to be called
+                await websocketClient.SendAsync(
+                    new ArraySegment<byte>(Encoding.UTF8.GetBytes("Hello World")),
+                    WebSocketMessageType.Binary,
+                    true,
+                    cancellationToken.Token);
+            });
+
+            Console.WriteLine("Press <enter> to exit");
+            Console.Read();
+
+            // close the connection nicely
+            websocketClient.CloseAsync(WebSocketCloseStatus.NormalClosure, "Bye", cancellationToken.Token);
+
+            // this will cause OnError on the server if not closed first
+            cancellationToken.Cancel();
+        }
+    }
+}
---
+++ websockets/FleckExample/FleckExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using Fleck;
+
+namespace FleckExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var websocketServer = new WebSocketServer("ws://0.0.0.0:8181");
+            websocketServer.Start(connection =>
+            {
+                connection.OnOpen = () => Console.WriteLine("OnOpen");
+                connection.OnClose = () => Console.WriteLine("OnClose");
+                connection.OnMessage = message =>
+                {
+                    Console.WriteLine($"OnMessage {message}");
+                    connection.Send($"Echo: {message}");
+                };
+                connection.OnBinary = bytes => Console.WriteLine($"OnBinary {Encoding.UTF8.GetString(bytes)}");
+                connection.OnError = exception => Console.WriteLine($"OnError {exception.Message}");
+                connection.OnPing = bytes => Console.WriteLine("OnPing");
+                connection.OnPong = bytes => Console.WriteLine("OnPong");
+            });
+
+            Console.WriteLine("Press <enter> to exit");
+            Console.Read();
+
+            websocketServer.Dispose();
+        }
+    }
+}
