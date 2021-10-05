91e2202d15d84a376407a5269ed1a261702c4642
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2021-10-05 22:19:54 +0100

    Added biometrics/fingerprint for Xamarin Forms code.

---
+++ BiometricTest/BiometricTest/BiometricTest/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+using Xamarin.Forms;
+
+namespace BiometricTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            if (await CrossFingerprint.Current.IsAvailableAsync(true))
+            {
+                var result = await CrossFingerprint.Current.AuthenticateAsync(
+                    new AuthenticationRequestConfiguration("Login", "Access your account"));
+                if (result.Authenticated)
+                {
+                    await DisplayAlert("Success", "Authenticated", "Ok");
+                }
+                else
+                {
+                    await DisplayAlert("Failure", "Not Authenticated", "Ok");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Failure", "Biometrics not available", "Ok");
+            }
+        }
+    }
+}
