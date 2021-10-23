2d012f43f1ccd3f619a92170eae4fcc67732df09
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2021-10-23 11:58:23 +0100

    Code for post http://putridparrot.com/blog/mocking-justmock-lite-nsubstitute-fakeiteasy-and-moq/

---
+++ Mocking/Mocking/SystemUnderTest/TradeRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Mocking.SystemUnderTest
+{
+    public class TradeRepository
+    {
+        private readonly IDataProvider _dataProvider;
+        private IList<string> _tradeIds;
+
+        public TradeRepository(IDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+            _dataProvider.Updates += Updates;
+        }
+
+        private void Updates(object sender, System.EventArgs e)
+        {
+            ReceivedUpdates = true;
+        }
+
+        public bool ReceivedUpdates { get; private set; }
+
+        public IList<string> GetTradeIds()
+        {
+            return _tradeIds ??= _dataProvider.GetIds("/trades");
+        }
+
+        public IList<string> BadGetTradeIds()
+        {
+            _dataProvider.GetIds("/trades");
+
+            return _tradeIds ??= _dataProvider.GetIds("/trades");
+        }
+    }
+}
