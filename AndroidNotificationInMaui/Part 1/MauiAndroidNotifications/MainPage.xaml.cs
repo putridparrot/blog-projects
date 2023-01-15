24d3bcd44dc2eaaddcecc45a9fbe6cd71aab15ae
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2023-01-15 11:47:47 +0000

    Part 1 of the notification series of blog posts for Android see https://putridparrot.com/blog/android-notifications-using-maui-part-1-of-10/

---
+++ AndroidNotificationInMaui/Part 1/MauiAndroidNotifications/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+
+namespace MauiAndroidNotifications;
+
+public partial class MainPage : ContentPage
+{
+    private readonly IMessenger _messenger;
+
+    public MainPage(IMessenger messenger)
+	{
+		InitializeComponent();
+
+        _messenger = messenger;
+    }
+
+    private void Channel1_OnClicked(object sender, EventArgs e)
+    {
+        _messenger.Send(new MessageData(1, Title.Text, Message.Text));
+    }
+
+    private void Channel2_OnClicked(object sender, EventArgs e)
+    {
+        _messenger.Send(new MessageData(2, Title.Text, Message.Text));
+    }
+
+}
+
---
+++ AndroidNotificationInMaui/Part 1/MauiAndroidNotifications/Platforms/Android/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using AndroidX.Core.App;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace MauiAndroidNotifications;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+    private NotificationManagerCompat _notificationManager;
+
+    public MainActivity()
+    {
+        var messenger = MauiApplication.Current.Services.GetService<IMessenger>();
+
+        messenger.Register<MessageData>(this, (recipient, message) =>
+        {
+            if (message.Channel == 1)
+            {
+                // channel 1
+                SendOnChannel1(message.Title, message.Message);
+            }
+            else
+            {
+                // channel 2
+                SendOnChannel2(message.Title, message.Message);
+            }
+        });
+    }
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        _notificationManager = NotificationManagerCompat.From(this);
+    }
+
+    private void SendOnChannel1(string title, string message)
+    {
+        var notification = new NotificationCompat.Builder(this, MainApplication.Channel1Id)
+            .SetSmallIcon(Resource.Drawable.abc_ab_share_pack_mtrl_alpha)
+            .SetContentTitle(title)
+            .SetContentText(message)
+            .SetPriority(NotificationCompat.PriorityHigh)
+            .SetCategory(NotificationCompat.CategoryMessage)
+            .Build();
+
+        _notificationManager.Notify(1, notification);
+    }
+
+    private void SendOnChannel2(string title, string message)
+    {
+        var notification = new NotificationCompat.Builder(this, MainApplication.Channel2Id)
+            .SetSmallIcon(Resource.Drawable.abc_btn_check_material)
+            .SetContentTitle(title)
+            .SetContentText(message)
+            .SetPriority(NotificationCompat.PriorityLow)
+            .Build();
+
+        _notificationManager.Notify(2, notification);
+    }
+}
