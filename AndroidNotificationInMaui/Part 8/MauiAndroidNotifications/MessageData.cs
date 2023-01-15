d4c09d8a592766fd0150870dba64161cbf0b028b
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2023-01-15 12:56:39 +0000

    Part 8 of the notification series of blog posts for Android see https://putridparrot.com/blog/android-notifications-using-maui-part-8-of-10/

---
+++ AndroidNotificationInMaui/Part 8/MauiAndroidNotifications/MessageData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAndroidNotifications;
+
+public record MessageData(int Channel, string Title, string Message);
---
+++ AndroidNotificationInMaui/Part 8/MauiAndroidNotifications/Platforms/Android/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAndroidNotifications.Platforms.Android;
+
+public class Message
+{
+    public Message(string text, string sender)
+    {
+        Text = text;
+        Sender = sender;
+        // The Timestamp is required for the NotificationCompat.MessagingStyle.Message object, so we'll just generate here
+        Timestamp = DateTime.Now.Millisecond; // prob. doesn't do the same as the Java example, need to check System.currentTimeMillis()
+    }
+
+    public string Text { get; }
+    public long Timestamp { get; }
+    public string Sender { get; }
+}
