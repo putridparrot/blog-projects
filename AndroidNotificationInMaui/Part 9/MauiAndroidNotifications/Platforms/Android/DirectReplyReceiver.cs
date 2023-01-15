39c2825d2e3a65478e976c7806a80eacbcfb4b85
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2023-01-15 13:15:59 +0000

    Part 9 of the notification series of blog posts for Android see https://putridparrot.com/blog/android-notifications-using-maui-part-9-of-10/

---
+++ AndroidNotificationInMaui/Part 9/MauiAndroidNotifications/Platforms/Android/DirectReplyReceiver.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Widget;
+using AndroidX.Core.App;
+using MauiAndroidNotifications.Platforms.Android;
+
+namespace MauiAndroidNotifications;
+
+// https://learn.microsoft.com/en-us/xamarin/android/app-fundamentals/broadcast-receivers
+[BroadcastReceiver(Enabled = true, Exported = false)]
+public class DirectReplyReceiver : BroadcastReceiver
+{
+    public override void OnReceive(Context context, Intent intent)
+    {
+        var remoteInput = RemoteInput.GetResultsFromIntent(intent);
+        if (remoteInput != null)
+        {
+            var replyText = remoteInput.GetCharSequence("key_text_reply");
+            var answer = new Message(replyText, null);
+            MainActivity.Messages.Add(answer);
+
+            // without calling this the message will get added to the Messages but left in limbo
+            // the reply will look like it's stuck sending a message (i.e. spinning progress bar and no updates)
+            MainActivity.SendOnChannel1(context);
+        }
+    }
+}
---
+++ AndroidNotificationInMaui/Part 9/MauiAndroidNotifications/Platforms/Android/MainApplication.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+
+namespace MauiAndroidNotifications;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+    public static readonly string Group1Id = "group1";
+    public static readonly string Group2Id = "group2";
+    public static readonly string Channel1Id = "channel1";
+    public static readonly string Channel2Id = "channel2";
+    public static readonly string Channel3Id = "channel3";
+    public static readonly string Channel4Id = "channel4";
+
+    public static readonly string ToastMessage = "toastMessage";
+
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+    public override void OnCreate()
+    {
+        base.OnCreate();
+
+        CreateNotificationChannels();
+    }
+
+    private void CreateNotificationChannels()
+    {
+        if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+        {
+#pragma warning disable CA1416
+            var group1 = new NotificationChannelGroup(Group1Id, "Group 1");
+
+            var group2 = new NotificationChannelGroup(Group2Id, "Group 2");
+
+            var channel1 = new NotificationChannel(Channel1Id, "Channel 1", NotificationImportance.High);
+            channel1.Description = "This is Channel 1";
+            channel1.Group = Group1Id;
+
+            var channel2 = new NotificationChannel(Channel2Id, "Channel 2", NotificationImportance.Low);
+            channel2.Description = "This is Channel 2";
+            channel2.Group = Group1Id;
+
+            var channel3 = new NotificationChannel(Channel3Id, "Channel 3", NotificationImportance.High);
+            channel3.Description = "This is Channel 3";
+            channel3.Group = Group2Id;
+
+            var channel4 = new NotificationChannel(Channel4Id, "Channel 4", NotificationImportance.Low);
+            channel4.Description = "This is Channel 4";
+            // purposefully no group
+
+            if (GetSystemService(NotificationService) is NotificationManager manager)
+            {
+                manager.CreateNotificationChannelGroup(group1);
+                manager.CreateNotificationChannelGroup(group2);
+
+                manager.CreateNotificationChannel(channel1);
+                manager.CreateNotificationChannel(channel2);
+                manager.CreateNotificationChannel(channel3);
+                manager.CreateNotificationChannel(channel4);
+            }
+#pragma warning restore CA1416
+        }
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
