acd479415d6bbffb4e4ed88c22c57e2dfeca1c4d
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2023-11-19 11:58:20 +0000

    Added code for post https://putridparrot.com/blog/working-with-kafka-host-via-docker-from-a-c-client/

---
+++ kafka-test/Producer/Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+
+var config = new List<KeyValuePair<string, string>>
+        {
+            new("bootstrap.servers", "192.168.0.88:19092"),
+            new("group.id", "my-group"),
+            new("auto.offset.reset", "earliest")
+        };
+
+const string topic = "my-topic";
+
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true; // prevent the process from terminating.
+    cts.Cancel();
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config)
+    .SetPartitionsAssignedHandler((c, partitions) =>
+    {
+        // reset the offsets for this client
+        var offsets = partitions.Select(tp => new TopicPartitionOffset(tp, Offset.Beginning));
+        return offsets;
+    })
+    .Build();
+
+consumer.Subscribe(topic);
+try
+{
+    while (true)
+    {
+        var cr = consumer.Consume(cts.Token);
+        Console.WriteLine($"Consumed event, topic {topic}: key = {cr.Message.Key} value = {cr.Message.Value}");
+    }
+}
+catch (OperationCanceledException)
+{
+    // Ctrl-C was pressed.
+}
+finally
+{
+    consumer.Close();
+}
---
+++ kafka-test/Producer/Producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using Confluent.Kafka;
+
+var config = new List<KeyValuePair<string, string>>
+{
+    new("bootstrap.servers", "192.168.0.88:19092"),
+    new("client.id", "my-producer")
+};
+
+const string topic = "my-topic";
+
+string[] tickers = { "AAPL", "GOOGL", "MSFT", "AMZN", "META", "TSLA", "GS" };
+string[] trades = { "Buy 100", "Sell 1000", "Buy 9090", "Sell 45", "Buy 900000", "Sell 123", "Buy 8901" };
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var rnd = new Random();
+
+for (var i = 0; i < 10; ++i)
+{
+    var ticker = tickers[rnd.Next(tickers.Length)];
+    var trade = trades[rnd.Next(trades.Length)];
+
+    producer.Produce(topic, new Message<string, string> { Key = ticker, Value = trade },
+        deliveryReport =>
+        {
+            if (deliveryReport.Error.Code != ErrorCode.NoError)
+            {
+                Console.WriteLine($"Error sending event: {deliveryReport.Error.Reason}");
+            }
+            else
+            {
+                Console.WriteLine($"Sent event topic = {topic}: key = {ticker} value = {trade}");
+            }
+        });
+}
+
+producer.Flush(TimeSpan.FromSeconds(10));
