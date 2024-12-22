2b5fbb746a6a26507751d0ce73014c5c7115808c
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2024-12-22 12:46:28 +0000

    Added code for https://putridparrot.com/blog/blazor-templates/

---
+++ BlazorTemplates/BlazorTemplates/Components/ListView.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorTemplates.Components;
+
+public partial class ListView<TItem>
+{
+    [Parameter] public RenderFragment? HeaderTemplate { get; set; }
+    [Parameter] public RenderFragment? FooterTemplate { get; set; }
+    [Parameter] public RenderFragment<RenderFragment>? ListTemplate { get; set; }
+    [Parameter] public RenderFragment<TItem> ItemTemplate { get; set; } = default!;
+    [Parameter] public IReadOnlyList<TItem> Items { get; set; } = default!;
+}
